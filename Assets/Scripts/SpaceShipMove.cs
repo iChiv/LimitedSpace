1a28cd8b5e8fbee9bbb91a1bc295e540bd5c49a4
Alex Ziwei Niu <zzzv0325@gmail.com>
2024-01-06 10:23:25 +0000

    wasd instant move instead tank turning && UI bug fixed

--- Assets/Scripts/SpaceShipMove.cs
+++ Assets/Scripts/SpaceShipMove.cs
@@ -24,35 +24,41 @@ public class SpaceShipMove : MonoBehaviour
     {
         float speedX = Input.GetAxis("Horizontal");
         float speedY = Input.GetAxis("Vertical");
-
-        //Test of moving instead of turning
-        // Vector2 moveDirection = new Vector2(speedX, speedY).normalized;
-        //
-        // if (moveDirection != Vector2.zero)
-        // {
-        //     float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90f;
-        //     float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.fixedDeltaTime);
-        //     
-        //     _rb.MoveRotation(angle);
-        //     _rb.MovePosition(_rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
-        // }
         
-        if (speedY != 0)
+        //wasd move
+        Vector2 moveDirection = new Vector2(speedX, speedY).normalized;
+        if (moveDirection != Vector2.zero)
         {
-            _rb.AddForce(transform.up * speedY * moveSpeed);
             engineFire.SetActive(true);
+            
+            _rb.AddForce(moveDirection * moveSpeed);
+            
+            float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90f;
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.fixedDeltaTime);
+            _rb.MoveRotation(angle);
         }
         else
         {
             engineFire.SetActive(false);
         }
         
-        _rb.angularVelocity = Mathf.Clamp(_rb.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
-        
-        if (speedX != 0 && Mathf.Abs(_rb.angularVelocity) < maxAngularVelocity)
-        {
-            _rb.AddTorque(-speedX * turnSpeed);
-        }
+        //ws move & ad turn
+        // if (speedY != 0)
+        // {
+        //     _rb.AddForce(transform.up * speedY * moveSpeed);
+        //     engineFire.SetActive(true);
+        // }
+        // else
+        // {
+        //     engineFire.SetActive(false);
+        // }
+        //
+        // _rb.angularVelocity = Mathf.Clamp(_rb.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+        //
+        // if (speedX != 0 && Mathf.Abs(_rb.angularVelocity) < maxAngularVelocity)
+        // {
+        //     _rb.AddTorque(-speedX * turnSpeed);
+        // }
     }
 
     private void Update()
